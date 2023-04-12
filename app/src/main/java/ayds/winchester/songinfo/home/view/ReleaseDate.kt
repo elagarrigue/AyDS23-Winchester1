f462e4da89686429daa0f94e8a62ca0b12a858c7
David López <dalo_vidpez@hotmail.com.ar>
2023-04-12 20:21:15 -0300

    Modificada clase releaseDate. Implementada con enumerados, renombradas variables e implementada factory

--- app/src/main/java/ayds/winchester/songinfo/home/view/ReleaseDate.kt
+++ app/src/main/java/ayds/winchester/songinfo/home/view/ReleaseDate.kt
@@ -3,13 +3,23 @@ package ayds.winchester.songinfo.home.view
 import java.text.SimpleDateFormat
 import java.util.*
 
-object ReleaseDateFactory {
-    fun get(precision: String, date: String) =
+
+enum class Precision{
+    day,
+    month,
+    year
+}
+interface ReleaseDateFactory {
+    fun get(precision: String, date: String): ReleaseDate
+}
+internal class ReleaseDateFactoryImpl : ReleaseDateFactory {
+
+    override fun get(precision: String, date: String) =
         when (precision) {
             "day" -> DayDate(date)
             "month" -> MonthDate(date)
-            //"year" -> YearDate(date)
-            else -> YearDate(date)
+            "year" -> YearDate(date)
+            else -> DefaultDate(date)
         }
 }
 
@@ -23,8 +33,8 @@ class DayDate(
     date: String
 ) : ReleaseDate(date) {
     override fun getFormat(): String {
-        val fechaConcreta: Date = SimpleDateFormat("yyyy-MM-dd").parse(date)
-        return SimpleDateFormat("dd/MM/yyyy").format(fechaConcreta)
+        val specificDate: Date = SimpleDateFormat("yyyy-MM-dd").parse(date)
+        return SimpleDateFormat("dd/MM/yyyy").format(specificDate)
     }
 }
 
@@ -41,16 +51,26 @@ class YearDate(
     date: String
 ) : ReleaseDate(date) {
     override fun getFormat(): String {
-        val fechaEntero: Int = date.toInt();
-        var fechaNueva: String = date;
-        if (añoBisiesto(fechaEntero))
-            fechaNueva += " (leap year)"
-        else
-            fechaNueva += " (not a leap year) "
+        val intDate: Int = date.toInt();
+        var newDate: String = date;
 
-        return fechaNueva
+        if (leapYear(intDate))
+            newDate += " (leap year)"
+        else
+            newDate += " (not a leap year) "
+
+        return newDate
     }
 
-    private fun añoBisiesto(n: Int) = ((n % 4 == 0 && n % 100 != 0) || n % 400 == 0)
+    private fun leapYear(n: Int) = ((n % 4 == 0 && n % 100 != 0) || n % 400 == 0)
+
+}
+
+class DefaultDate(
+    date: String
+): ReleaseDate(date){
+    override fun getFormat(): String {
+        return date
+    }
 
 }
