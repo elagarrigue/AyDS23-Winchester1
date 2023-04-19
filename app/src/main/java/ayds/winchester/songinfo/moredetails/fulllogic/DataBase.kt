39a2077f2cb362a5491341dd9cf976c034b3b664
David López <dalo_vidpez@hotmail.com.ar>
2023-04-19 12:20:32 -0300

    Hecho refactoring de función testDB()

--- app/src/main/java/ayds/winchester/songinfo/moredetails/fulllogic/DataBase.kt
+++ app/src/main/java/ayds/winchester/songinfo/moredetails/fulllogic/DataBase.kt
@@ -5,11 +5,10 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.util.Log
-import java.sql.Connection
-import java.sql.DriverManager
-import java.sql.SQLException
+import java.sql.*
 
 class DataBase(context: Context?) : SQLiteOpenHelper(context, "dictionary.db", null, 1) {
+
     override fun onCreate(db: SQLiteDatabase) {
         db.execSQL(
             "create table artists (id INTEGER PRIMARY KEY AUTOINCREMENT, artist string, info string, source integer)"
@@ -20,38 +19,51 @@ class DataBase(context: Context?) : SQLiteOpenHelper(context, "dictionary.db", n
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {}
 
     companion object {
-        fun testDB() {
-            var connection: Connection? = null
-            try {
-                // create a database connection
-                connection = DriverManager.getConnection("jdbc:sqlite:./dictionary.db")
-                val statement = connection.createStatement()
-                statement.queryTimeout = 30 // set timeout to 30 sec.
+        val TIMEOUT = 30;
+        val URL_CONNECTION = "jdbc:sqlite:./dictionary.db"
+        val ID_COLUMN = "id"
+        val ARTIST_COLUMN = "artist"
+        val INFO_COLUMN = "info"
+        val SOURCE_COLUMN = "source"
+        var artistQuery = "select * from artists"
 
-                //statement.executeUpdate("drop table if exists person");
-                //statement.executeUpdate("create table person (id integer, name string)");
-                //statement.executeUpdate("insert into person values(1, 'leo')");
-                //statement.executeUpdate("insert into person values(2, 'yui')");
-                val rs = statement.executeQuery("select * from artists")
-                while (rs.next()) {
-                    // read the result set
-                    println("id = " + rs.getInt("id"))
-                    println("artist = " + rs.getString("artist"))
-                    println("info = " + rs.getString("info"))
-                    println("source = " + rs.getString("source"))
-                }
+        fun testDB() {
+            try {
+                var connection = createConnection(URL_CONNECTION)
+                val statement = prepareStatement(connection)
+                val rs = executeQuery(statement, artistQuery)
+                printResults(rs)
+                closeConnection(connection)
             } catch (e: SQLException) {
-                // if the error message is "out of memory",
-                // it probably means no database file is found
                 System.err.println(e.message)
-            } finally {
-                try {
-                    connection?.close()
-                } catch (e: SQLException) {
-                    // connection close failed.
-                    System.err.println(e)
-                }
             }
+        }
+
+        fun createConnection(url: String): Connection{
+            return DriverManager.getConnection(url);
+        }
+
+        fun prepareStatement(connection: Connection) : Statement {
+            var statement = connection.createStatement()
+            statement.queryTimeout = TIMEOUT
+            return statement
+        }
+
+        fun executeQuery(statement: Statement, query: String): ResultSet {
+            return statement.executeQuery(query)
+        }
+
+        fun printResults(rs: ResultSet){
+            while (rs.next()) {
+                println("id = " + rs.getInt(ID_COLUMN))
+                println("artist = " + rs.getString(ARTIST_COLUMN))
+                println("info = " + rs.getString(INFO_COLUMN))
+                println("source = " + rs.getString(SOURCE_COLUMN))
+            }
+        }
+
+        fun closeConnection(connection: Connection){
+            connection.close()
         }
 
         @JvmStatic
@@ -107,4 +119,5 @@ class DataBase(context: Context?) : SQLiteOpenHelper(context, "dictionary.db", n
             return if (items.isEmpty()) null else items[0]
         }
     }
-}
+
+    }
